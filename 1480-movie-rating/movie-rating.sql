d88ec19e2bbf85d721e5b625ee7fac0e6a59d7ae
Harsh M Singh <manojrsingh1974@gmail.com>
2026-07-28 21:13:02 +0530

    Time: 1778 ms (9.24%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1480-movie-rating/movie-rating.sql
@@ -0,0 +1,16 @@
+(SELECT u.name AS results
+FROM MovieRating r
+JOIN Users u ON r.user_id = u.user_id
+GROUP BY r.user_id
+ORDER BY COUNT(*) DESC, u.name ASC
+LIMIT 1)
+
+UNION ALL
+
+(SELECT m.title AS results
+FROM MovieRating r
+JOIN Movies m ON r.movie_id = m.movie_id
+WHERE r.created_at BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY r.movie_id
+ORDER BY AVG(r.rating) DESC, m.title ASC
+LIMIT 1);

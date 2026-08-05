cc7d32f204d8011778307942996c231458edf54b
Harsh M Singh <manojrsingh1974@gmail.com>
2026-08-05 21:23:23 +0530

    Time: 813 ms (13.19%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,9 @@
+SELECT ROUND(
+    (SELECT COUNT(f.player_id)
+     FROM (SELECT player_id, MIN(event_date) AS first_login 
+           FROM Activity GROUP BY player_id) f 
+     JOIN Activity a ON f.player_id = a.player_id 
+         AND a.event_date = f.first_login + INTERVAL 1 DAY)
+    /
+    (SELECT COUNT(DISTINCT player_id) FROM Activity)
+, 2) AS fraction;
